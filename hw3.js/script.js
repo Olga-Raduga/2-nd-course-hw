a1689422bd305d7a3cc57504582cf91de22b0958
Olga-Raduga <9431514@gmail.com>
2026-05-02 14:20:25 +0300

    задание 6: Остаток от деления 9 на 2

--- hw3.js/script.js
+++ hw3.js/script.js
@@ -21,4 +21,17 @@ alert(x + b);
 alert(x - b);
 alert(x * b);
 alert(x / b);
- 
+//задание 5
+//Возведение 2 в 5-ю степень
+let result = 2 ** 5;
+alert(result); //32
+//задание 6
+//Остаток от деления 9 на 2
+{
+    let a = 9;
+    let b = 2;
+    alert(a % b);
+}
+
+
+
