20264a62c79a7437c6674324e207e001911eab15
Olga-Raduga <9431514@gmail.com>
2026-05-14 00:16:21 +0300

    hw6.js.задание 1, Функция корректно возвращает меньшее из двух чисел или одно из них, если числа равны.

---
+++ hw6.js/script.js
@@ -0,0 +1,14 @@
+
+//задание 1
+//меньшее из двух чисел
+function getMin(a, b) {
+    if (a <= b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+console.log(getMin(8, 4));
+console.log(getMin(6, 6));
+console.log(getMin(3, 9));
+
