ed97e241a1613bd1e4928ce6cdfc2aeba4cc4ebd
Olga-Raduga <9431514@gmail.com>
2026-05-14 00:19:26 +0300

    hw6.js.задание 2, Функция корректно определяет четность или нечетность числа и возвращает соответствующую строку.

--- hw6.js/script.js
+++ hw6.js/script.js
@@ -12,3 +12,16 @@ console.log(getMin(8, 4));
 console.log(getMin(6, 6));
 console.log(getMin(3, 9));
 
+//задание 2
+//четное или нечетное
+function checkEven(n) {
+    if (n % 2 === 0) {
+        return "Число четное";
+    } else {
+        return "Число нечетное";
+    }
+}
+console.log(checkEven(4));
+console.log(checkEven(7));
+console.log(checkEven(0));
+
