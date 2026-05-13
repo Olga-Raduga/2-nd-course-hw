c09d20a4e71d0df03f0a0cdfd4a9df077231f506
Olga-Raduga <9431514@gmail.com>
2026-05-14 00:46:29 +0300

    hw6.js.задание 4,Функция корректно запрашивает возраст и выводит соответствующее сообщение на экран для всех случаев.

--- hw6.js/script.js
+++ hw6.js/script.js
@@ -25,7 +25,7 @@ console.log(checkEven(4));
 console.log(checkEven(7));
 console.log(checkEven(0));
 
-//задание 2
+//задание 3
 //функция-квадрат числа
 function printSquare(num) {
     console.log(num * num);
@@ -37,3 +37,17 @@ printSquare(5);
 let result = getSquare(5);
 console.log(result);
 console.log(result + 10);
+
+//задание 4
+//Возраст пользователя
+function checkAge() {
+let age = Number(prompt("Сколько вам лет?"));
+if (age < 0) {
+    alert("Вы ввели неправильное значение");
+} else if (age >= 0 && age <= 12) {
+    alert("Привет, друг!");
+} else {
+    alert("Добро пожаловать!");
+}
+}
+checkAge();
