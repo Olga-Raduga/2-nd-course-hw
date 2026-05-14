10ed91286cdb061d46afcb0660eb2a6f50834c26
Olga-Raduga <9431514@gmail.com>
2026-05-15 00:36:45 +0300

    hv7, основное задание по созданию 2й игры выполнено.Сайт генерирует случайные задачи и запрашивает ответ

--- hw3/script.js
+++ hw3/script.js
@@ -24,4 +24,36 @@ function guessNumberGame() {
     }
 }
 
+function arithmeticGame() {
+    const operations = ['+', '-', '*', '/'];
+    const operation = operations[Math.floor(Math.random() * operations.length)];
+    let num1, num2, correctAnswer;
+    if (operation === '+') {
+        num1 = Math.floor(Math.random() * 20) + 1;
+        num2 = Math.floor(Math.random() * 20) + 1;
+        correctAnswer = num1 + num2;
+    } else if (operation === '-') {
+        num1 = Math.floor(Math.random() * 20) + 1;
+        num2 = Math.floor(Math.random() * num1) + 1;
+        correctAnswer = num1 - num2;
+    } else if (operation === '*') {
+        num1 = Math.floor(Math.random() * 10) + 1;
+        num2 = Math.floor(Math.random() * 10) + 1;
+        correctAnswer = num1 * num2;
+    } else {
+        num2 = Math.floor(Math.random() * 9) + 1;
+        correctAnswer = Math.floor(Math.random() * 10) + 1;
+        num1 = num2 * correctAnswer;
+    }
+    const userAnswer = prompt(`Реши пример: ${num1} ${operation} ${num2} = Ваш ответ?`);
+    if (userAnswer === null) {
+        alert("Игра прервана. Жаль, что вы уходите!");
+        return;
+    }
+    if (parseInt(userAnswer) === correctAnswer) {
+        alert('Отлично! Ваш ответ верный!');
+    } else {
+        alert(`Ошибка! Ваш ответ: ${userAnswer}. Правильный ответ: ${correctAnswer}`);
+    }
+}
 
