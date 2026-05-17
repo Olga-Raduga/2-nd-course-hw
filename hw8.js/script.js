30ce676a62c8f96d7ec62f6345d88b4911b196db
Olga-Raduga <9431514@gmail.com>
2026-05-17 18:37:48 +0300

    hw8, задание 1,Строка 'js' преобразована в верхний регистр

---
+++ hw8.js/script.js
@@ -0,0 +1,8 @@
+//задание 1
+//Преобразовываем строку ‘js’ в верхний регистр
+const originalString = 'js';
+const upperCaseString = originalString.toUpperCase();
+console.log("Задание 1:");
+console.log(`Исходная строка: '${originalString}', в верхнем регистре: '${upperCaseString}'`);
+//2й вариант
+console.log('js'.toUpperCase());
