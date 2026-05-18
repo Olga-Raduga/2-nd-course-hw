a7f2f76cca6cac254bd7075e445510bb7571780d
Olga-Raduga <9431514@gmail.com>
2026-05-18 22:12:32 +0300

    hw9.js, задание 2, Функция filter реализована, работает аналогично методу массива filter, возвращает новый массив с элементами, для которых функция-правило возвращает true

--- hw9.js/script.js
+++ hw9.js/script.js
@@ -12,3 +12,34 @@ people.sort(function (a, b) {
     return a.age - b.age;
 });
 console.log("Отсортированный массив (по возрастанию возраста):", people);
+//задание 2
+//Реализация собственной функции
+function isPositive(number) {
+    return number > 0;
+}
+function isMale(person) {
+    return person.gender === 'male';
+}
+function customFilter(array, ruleFunction) {
+    const filteredArray = [];
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        if (ruleFunction(element)) {
+            filteredArray.push(element);
+        }
+    }
+    return filteredArray;
+}
+console.log("\nЗадание 2:");
+const numbersForFilter = [3, -4, 1, 9];
+console.log("Исходный массив чисел:", numbersForFilter);
+console.log("Фильтрация по положительным числам:", customFilter(numbersForFilter, isPositive));
+
+const peopleForFilter = [
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
+];
+console.log("\nИсходный массив людей:", peopleForFilter);
+console.log("Фильтрация по мужскому полу:", customFilter(peopleForFilter, isMale));
