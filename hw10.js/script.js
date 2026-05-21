9975b38210bea986aafa7866e69b9d76359ccf76
Olga-Raduga <9431514@gmail.com>
2026-05-22 01:22:11 +0300

    hw10, задание 3, Созданы заголовок и кнопка, которая меняет текст в заголовке

--- hw10.js/script.js
+++ hw10.js/script.js
@@ -19,3 +19,11 @@ const changeColorButton2 = document.querySelector('#changeColorButton2');
 changeColorButton2.addEventListener('click', function () {
     paragraph2.style.color = 'blue';
 });
+
+//задание 3
+//Динамическое изменение текста
+const heading3 = document.querySelector('#myHeading3');
+const changeTextButton3 = document.querySelector('#changeTextButton3');
+changeTextButton3.addEventListener('click', function () {
+    heading3.textContent = 'Привет, мир!';
+});
